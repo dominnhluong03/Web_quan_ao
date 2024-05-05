13a0a5fc1df8b0bbef41e4659cb1bb4e3c3ba0ae
dominhluong03 <luongcris007@gmail.com>
2024-05-05 19:59:09 +0700

    Web quan ao

---
+++ Web_quan_ao/cart.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/cart.css">
+    <link rel="stylesheet" href="/css/responsive.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <title>Giỏ hàng của bạn</title>
+    <link rel="shortcut icon" href="/image/Logo/main-logo.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="fluid-container">
+        <!-- cart -->
+        <div class="layoutPage-cart" style="margin-top: 150px;">
+            <div class="heading-page">
+                <div class="header-page">
+                    <h1 style="margin-left: 40%;">Giỏ hàng của bạn</h1>
+                    <div class="warning" >
+                        <p style="margin-left: 20%;">Sản phẩm mua được phép đổi trong vòng 7 ngày (tính từ ngày quý khách nhận được hàng). Chúng tôi không chấp nhận hủy đơn hàng.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container" style="padding-bottom: 100px;">
+                <div class="row wrapbox-content-cart">
+                    <div class="col-md-8 col-sm-8 col-xs-12 contentCart-detail">
+                        <div class="cart-container">
+                            <div class="expanded-message">
+                                <table class="table-cart">
+                                    <tbody>
+                                        <tr class="line-item-container" data-variant-id="">
+                                            <td class="image">
+                                                <div class="product_image">
+                                                    <a href="" >
+                                                        <img style="height: 200px;" src="  //product.hstatic.net/1000402464/product/ks23fh53c-ls_blue____2__8d313764d57d46819d62cf899539f955_medium.jpg"
+                                                            alt="Áo Polo KS23FH53C-LS">
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td class="item">
+                                                <h3 style="margin-left: 30%"><a href="" style="color: #820012;">Áo Polo KS23FH53C-LS </a></h3>
+                                                <p style="margin-left: 30%">
+                                                    <span class="pri">980,000₫</span>
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-12 sidebarCart-sticky">
+                        <div class="sidebox-order">
+                            <div class="sidebox-order-inner">
+                                <div class="sidebox-order_title">
+                                    <h3>Thông tin đơn hàng</h3>
+                                </div>
+                                <div class="sidebox-order_total">
+                                    <p>Tổng tiền:
+                                        <span class="total-price">980.000đ</span>
+                                    </p>
+                                </div>
+                                <div class="sidebox-order_text">
+                                    <p>
+                                        Phí vận chuyển sẽ được tính ở trang thanh toán.
+                                        <br> Bạn cũng có thể nhập mã giảm giá ở trang thanh toán.
+                                    </p>
+                                </div>
+                                <div class="sidebox-order_action">
+                                    <a href="" class="button dark btncart-checkout">
+                                        THANH TOÁN
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-1 col-sm-1"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
